b5085ec35d92e6cd44b8489f6a91792690e4d217
144bharat <144bharatgautam@gmail.com>
2023-03-03 17:18:35 +0530

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,7 +18,8 @@ import { SimpleformComponent } from './simpleform/simpleform.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/simpleform/simpleform.component.ts
+++ src/app/simpleform/simpleform.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';        //NgForm used here as a form datatype in getForm() function.
 @Component({
   selector: 'app-simpleform',
   templateUrl: './simpleform.component.html',
   styleUrls: ['./simpleform.component.css']
 })
 export class SimpleformComponent {
-
+  displayData:any={};
+  getForm(data:NgForm)
+  {
+    this.displayData=data;
+    console.log(data);
+    console.log(this.displayData);
+    
+  }
 }
